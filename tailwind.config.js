6b61f6fecca32e7aec4244968a3f10cf52ed9b2f
Your Name <muhthoriq73@gmail.com>
2021-09-21 15:28:28 +0700

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{js,ts,vue,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
